c5d57ab91f3049b9d5a612c528e786ca9860d5a1
Terry <156342254+Terrymanzi@users.noreply.github.com>
2024-06-29 09:56:35 -0700

    Updated file for P8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -31,7 +31,7 @@ def display_todo_progress(employee_id):
     completed_tasks = [task for task in todos_data if task.get("completed")]
     number_of_done_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task.get('title')}")
 
